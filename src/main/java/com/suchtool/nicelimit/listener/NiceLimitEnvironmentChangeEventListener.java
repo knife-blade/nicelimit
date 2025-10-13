2db5576f946706becb3351e8e88996111c5e8bbf
knifeout <2716552863@qq.com>
2025-10-13 20:57:56 +0800

    1.0.15版本发布（只动态监听本组件相关配置）

--- src/main/java/com/suchtool/nicelimit/listener/NiceLimitEnvironmentChangeEventListener.java
+++ src/main/java/com/suchtool/nicelimit/listener/NiceLimitEnvironmentChangeEventListener.java
@@ -4,6 +4,9 @@ import com.suchtool.nicelimit.handler.NiceLimitHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Set;
 
 @Slf4j
 public class NiceLimitEnvironmentChangeEventListener implements ApplicationListener<EnvironmentChangeEvent> {
@@ -16,7 +19,20 @@ public class NiceLimitEnvironmentChangeEventListener implements ApplicationListe
     @Override
     public void onApplicationEvent(EnvironmentChangeEvent event) {
         try {
-            niceLimitHandler.doCheckAndUpdateConfig();
+            Set<String> keys = event.getKeys();
+            if (!CollectionUtils.isEmpty(keys)) {
+                boolean requireUpdateConfig = false;
+                for (String key : keys) {
+                    if (key.startsWith("suchtool.nicelimit")) {
+                        requireUpdateConfig = true;
+                        break;
+                    }
+                }
+
+                if (requireUpdateConfig) {
+                    niceLimitHandler.doCheckAndUpdateConfig();
+                }
+            }
         } catch (Exception e) {
             log.error("nicelimit EnvironmentChangeEventListener error", e);
         }
