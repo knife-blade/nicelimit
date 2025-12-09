942fd84172a01b5a1bc829c0f54ed7b4e1980c80
knifeout <2716552863@qq.com>
2025-12-09 18:10:08 +0800

    1.0.20版本发布（支持SpringBoot3）

---
+++ src/main/java/com/suchtool/nicelimit/listener/NiceLimitConfigChangedListener.java
@@ -0,0 +1,63 @@
+package com.suchtool.nicelimit.listener;
+
+import com.suchtool.nicelimit.handler.NiceLimitHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Set;
+
+@Aspect
+@Slf4j
+public class NiceLimitConfigChangedListener {
+    private final NiceLimitHandler niceLimitHandler;
+
+    public NiceLimitConfigChangedListener(NiceLimitHandler niceLimitHandler) {
+        this.niceLimitHandler = niceLimitHandler;
+    }
+
+    @Pointcut("execution(* org.springframework.cloud.context.properties.ConfigurationPropertiesRebinder.onApplicationEvent(..))")
+    public void pointcut() {
+
+    }
+
+    /**
+     * 在属性更新完后调用
+     */
+    @AfterReturning(
+            value = "pointcut()",
+            returning = "returnValue"
+    )
+    public void afterReturning(JoinPoint joinPoint, Object returnValue) {
+        try {
+            process(joinPoint);
+        } catch (Exception e) {
+            log.error("nicelimit EnvironmentChangeEventListener error", e);
+        }
+    }
+
+    private void process(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        EnvironmentChangeEvent event = (EnvironmentChangeEvent) args[0];
+
+
+        Set<String> keys = event.getKeys();
+        if (!CollectionUtils.isEmpty(keys)) {
+            boolean requireUpdateConfig = false;
+            for (String key : keys) {
+                if (key.startsWith("suchtoolnicelimit")) {
+                    requireUpdateConfig = true;
+                    break;
+                }
+            }
+
+            if (requireUpdateConfig) {
+                niceLimitHandler.doCheckAndUpdateConfig();
+            }
+        }
+    }
+}
